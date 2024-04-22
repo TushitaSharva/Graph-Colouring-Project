d38f727a817728f59cfcf6cde972fd8e9db113ff
TushitaSharva <tushitasharva03@gmail.com>
2024-04-22 16:39:56 +0530

    Bash file

---
+++ bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input_file="inp-params.txt"
+
+# Read the first number from the first line of the input file
+first_number=$(head -n 1 "$input_file" | awk '{print $1}')
+
+# Compile the C++ code with mpic++
+mpic++ algorithm.cpp -g -o a.exe
+
+# Run with x threads and append output to space-VC.log and space-SK.log
+mpirun --mca orte_base_help_aggregate 0 --oversubscribe -n $first_number ./a.exe
